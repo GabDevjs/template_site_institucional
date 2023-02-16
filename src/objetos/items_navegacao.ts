a212cd883d116da944f3236b9fd5ac80c7380e26
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-16 19:24:51 -0300

    [add] objestos com os items de navegação

---
+++ src/objetos/items_navegacao.ts
@@ -0,0 +1,10 @@
+import { IObjctItemsNavegacao } from "@/types/objetos/items_navegacao";
+import { AiOutlineHome } from 'react-icons/ai'
+
+export const ItemsNavegacao: IObjctItemsNavegacao = [
+    {
+        name: "Home",
+        icon: AiOutlineHome,
+        link: "/"
+    }
+]
