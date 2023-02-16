0d1281f4ef30cee5cff4b942d51dcf5b78a94786
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-16 19:25:24 -0300

    [add] objestos com os items de midias sociais

---
+++ src/objetos/social_medias.ts
@@ -0,0 +1,18 @@
+import { IObjctSocialMedias } from "@/types/objetos/social_medias";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
+
+export const socialMedias: IObjctSocialMedias = {
+    medias: [
+        {
+            name: "Facebook",
+            icon: BsFacebook,
+            link: "#"
+        },
+        {
+            name: "Instagram",
+            icon: BsInstagram,
+            link: "#"
+        }
+    ],
+    telefone: "00 00000-0000"
+}
