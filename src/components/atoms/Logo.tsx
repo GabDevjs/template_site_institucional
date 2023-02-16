5e905fa96b84bbd36ee79df816c23b9c9b2ed5aa
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-16 19:29:48 -0300

    [add] componente de logo do site

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export const Logo = () => {
-    return (
-        
-            <div className="flex items-center ">
-              <i className="w-10 h-10 bg-zinc-200 rounded-full ">
-
-              </i>
-            </div>
-    );
-  };
+  return (
+    <Link href="/">
+      <div className="flex items-center ">
+        <i className="h-10 w-10 rounded-full bg-zinc-400 "></i>
+      </div>
+    </Link>
+  );
+};
