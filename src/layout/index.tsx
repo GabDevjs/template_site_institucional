3b0687d0481ae316e9a32326712f2af807fddbe8
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-16 19:25:52 -0300

    [add] layout do site

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,30 +1,26 @@
-import {Navbar} from "@/components/organisms";
-import  Head  from "next/head";
-import React from "react";
+import { Footer } from "@/organisms/Footer";
+import { Navbar } from "@/organisms/Navbar";
+import Head from "next/head";
 import { LayoutProps } from "../types";
 
 export const Layout = (props: LayoutProps) => {
-    const { children, titlePages, descriptionPages, } = props;
+  const { children, titlePages, descriptionPages } = props;
 
-    return (
-        <>
-          <Head>
-            <meta
-              name="description"
-              content={descriptionPages ? descriptionPages : ""}
-            />
-            <title>
-              {titlePages
-                ? "" + titlePages
-                : "Pagina"}
-            </title>
-          </Head>
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content={descriptionPages ? descriptionPages : ""}
+        />
+        <title>{titlePages ? "" + titlePages : "Pagina"}</title>
+      </Head>
 
-          <Navbar/>
-          <main>
-            {children}
-          </main>
-
-      </>
-    )
-}
+      <Navbar />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
