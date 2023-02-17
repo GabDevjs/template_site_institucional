808f200cbb812c3c008b0234a6adb10f0936c438
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-17 09:50:09 -0300

    [add] aos no app global para as animações

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
+import type { AppProps } from "next/app";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "@/styles/globals.css";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return <Component {...pageProps} />;
 }
