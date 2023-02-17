c530b252e73696ef80a12436688ec1ed323def5c
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-17 09:51:43 -0300

    [add] componente global de widgets do site

---
+++ src/components/organisms/Widgets.tsx
@@ -0,0 +1,9 @@
+import { ButtonWhats } from "@/atoms/ButtonWhatsapp";
+
+export const Widgtes = () => {
+  return (
+    <div className="fixed bottom-8 right-10 hidden md:bottom-10 md:right-12  md:inline-block lg:right-20 xl:right-36">
+      <ButtonWhats />
+    </div>
+  );
+};
