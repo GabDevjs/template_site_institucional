d60ca63e452fac96a707f141224706fc4d2e5761
Gabriel <flaviogabrielsr0507@gmail.com>
2023-02-22 01:41:09 -0300

    [update] items da navegação do site

--- src/objetos/items_navegacao.ts
+++ src/objetos/items_navegacao.ts
@@ -1,10 +1,16 @@
 import { IObjctItemsNavegacao } from "@/types/objetos/items_navegacao";
 import { AiOutlineHome } from 'react-icons/ai'
+import { BsCircleFill } from "react-icons/bs";
 
 export const ItemsNavegacao: IObjctItemsNavegacao = [
     {
         name: "Home",
         icon: AiOutlineHome,
         link: "/"
+    },
+    {
+        name: "Blog",
+        icon: BsCircleFill,
+        link: "/blog"
     }
 ]
